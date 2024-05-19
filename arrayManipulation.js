be42c3f4ec9992bde06eaf03204f591cb6ee90e3
Hakeemzen <hkzenuwah@st.ug.edu.gh>
2024-05-19 20:25:33 +0000

    updating task 1

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -11,3 +11,18 @@ function processArray(array) {
     
     return result;
 }
+
+
+function processArray(array) {
+    var result = [];
+    
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] % 2 === 0) {
+            result.push(array[i] * array[i]);
+        } else {
+            result.push(array[i] * 3);
+        }
+    }
+    
+    return result;
+}
